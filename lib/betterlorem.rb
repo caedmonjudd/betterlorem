c13dbd546b68bc5b6db028bca68dcfb0589ca517
Caedmon Judd <caedmon@statebuilt.com>
2012-10-09 12:49:43 -0500

    Work

--- lib/betterlorem.rb
+++ lib/betterlorem.rb
@@ -7,8 +7,10 @@
 
 module BetterLorem
 
+  PUNCTUATION = ['.', ',', '!', '?', ':']
+
   # Return Words
-  def self.w(count)
+  def self.w(count, line_ending = "<br>", surround_with_tag = "p")
     loader = Loader.new
 
     # Merge paragraphs into one line and split into words
@@ -17,34 +19,75 @@ module BetterLorem
     # Start at a random index in the array
     start_inx = rand(words.count)
 
+    # Select a random subset of words
     select_words = words[start_inx, count]
+
+    # Capilalize the first word in the sentence
     select_words[0].to_s.capitalize!
 
+    # Merge words into a string
     return_words = select_words.join(' ')
 
-    loader.correct_punctuation(return_words)
-    return_words
+    # Correct the sentence's punctuation
+    correct_punctuation(return_words)
+
+    return_words = "<#{surround_with_tag}>#{return_words}</#{surround_with_tag}>#{line_ending}"
   end
 
   # Return Paragraphs
-  def self.p(count)
+  def self.p(count, line_ending = "<br>", surround_with_tag = "p")
     loader = Loader.new
+    start_inx = 1
 
+    # Start at a random index in the array but do not overrun array
+    start_inx = rand(loader.lines.count - count)
+
+    # Build paragraphs from array
+    paragraphs = loader.lines[start_inx, count]
+
+    # Build final format based on parameters
+    paragraphs.map! do |line|
+      line = "<#{surround_with_tag}>#{line}</#{surround_with_tag}>#{line_ending}"
+    end
+
+    paragraphs.join('\n')
   end
 
   # Return Characters
-  def self.c(count)
+  def self.c(count, line_ending = "<br>", surround_with_tag = "p")
     loader = Loader.new
+
+    # Start at a random index in the array
+
+
+    lines = loader.lines.join(' ')
+
+    start_inx = rand(lines.length)
+
+    chars = lines[start_inx, count]
+
+
 
   end
 
+
   private
+
+  # Determine if the line ends with punctuation
+  def self.correct_punctuation(line)
+    PUNCTUATION.each do |punct|
+      if line[line.length - 1].to_s == punct
+        line[line.length - 1] = "."
+        return
+      end
+    end
+    line << "."
+  end
+
 
   class Loader
 
     attr_accessor :lines
-
-    PUNCTUATION = ['.', ',', '!', '?', ':']
 
     def initialize
       self.lines = []
@@ -60,19 +103,9 @@ module BetterLorem
 
     # Capitalize the sentence that is passed
     def capilalize_sentences(line)
-      line.split.each { |x| print x.capitalize!, " "}.join(" ")
+      line.split.each { |x| print x.capitalize!, " " }.join(" ")
     end
 
-    # Determine if the line ends with punctuation
-    def correct_punctuation(line)
-      PUNCTUATION.each do |punct|
-        if line[line.length - 1].to_s == punct
-          line[line.length - 1] = "."
-          return
-        end
-      end
-      line << "."
-    end
 
   end
 
--- spec/betterlorem_spec.rb
+++ spec/betterlorem_spec.rb
@@ -6,21 +6,22 @@ describe BetterLorem do
   describe ".w" do
     it "should return words only" do
       words = BetterLorem.w(20)
-puts words
       words.should_not == ""
     end
   end
 
   describe ".p" do
     it "should return paragraphs only" do
-
-
-
+      lines = BetterLorem.p(3)
+      lines.should_not == ""
     end
   end
 
   describe ".c" do
     it "should return characters only" do
+      chars = BetterLorem.c(100)
+      puts chars
+      chars.should_not == ""
     end
   end
 
